08ea6ddfcf6e8dbab662751238a85651cef5bc2b
ramos nunes <ramosnunesjc1@gmail.com>
2026-02-09 00:30:50 -0300

    Inicial: projeto ForumHub com Spring Boot

---
+++ src/main/java/br/com/ramos/forumhub/ForumhubApplication.java
@@ -0,0 +1,12 @@
+package br.com.ramos.forumhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumhubApplication.class, args);
+    }
+}
---
+++ src/main/java/domain/topico/StatusTopico.java
@@ -0,0 +1,6 @@
+package br.com.ramos.forumhub.domain.topico;
+
+public enum StatusTopico {
+    ABERTO,
+    FECHADO
+}
